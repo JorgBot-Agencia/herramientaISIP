4f0fe0e7ee8a8cb0d829914de2ec841f01ea60f0
FredyCortes <frcortez@ab-sistemas.com>
2019-09-03 16:39:36 -0500

    Integracion interfaz 3,4,5

--- app/src/main/java/com/formato/isp/MainActivity.java
+++ app/src/main/java/com/formato/isp/MainActivity.java
@@ -2,13 +2,30 @@ package com.formato.isp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnIniciarSesion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnIniciarSesion = (Button) findViewById(R.id.btnIniciarSesion);
+        btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                iniciarSesion();
+            }
+        });
+    }
+    public void iniciarSesion(){
+        Intent intent = new Intent(this, principal.class);
+        startActivity(intent);
     }
 }
