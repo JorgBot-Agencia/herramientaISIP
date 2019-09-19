dc5f92eb8a7ef634fcea5802b0fcced9d4d39e7d
Huver Acevedo <hacevedo@ab-sistemas.com>
2019-09-19 16:10:54 -0500

    Integración vista

---
+++ app/src/main/java/com/formato/isp/infoDetallada.java
@@ -0,0 +1,14 @@
+package com.formato.isp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class infoDetallada extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_detallada);
+    }
+}
