0cc924315c5d37301dfbf7ffca1d28cc25ddc523
huver0 <hacevedo@ab-sistemas.com>
2019-09-19 17:14:29 -0500

    Vista 2.1.2.1.

--- app/src/main/java/com/formato/isp/infoDetallada.java
+++ app/src/main/java/com/formato/isp/infoDetallada.java
@@ -2,13 +2,28 @@ package com.formato.isp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class infoDetallada extends AppCompatActivity {
+
+    Button btnIniciar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_detallada);
+
+        btnIniciar = findViewById(R.id.btn_start);
+        btnIniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent abrirEncuesta = new Intent(view.getContext(), preguntasEncuesta.class);
+                startActivity(abrirEncuesta);
+            }
+        });
+
     }
 }
