7df87441e82b7e681d317b2b770163288a4c168e
huver0 <hacevedo@ab-sistemas.com>
2019-09-24 19:00:24 -0500

    Agregando petición http GET para obtener empresas desde la API1

--- app/src/main/java/com/formato/isp/GestionEmpresa/buscar_empresa.java
+++ app/src/main/java/com/formato/isp/GestionEmpresa/buscar_empresa.java
@@ -6,17 +6,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.formato.isp.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.PrintWriter;
 
 public class buscar_empresa extends AppCompatActivity {
 
     ImageView img;
+    private RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buscar_empresa);
+
+        queue = Volley.newRequestQueue(this);
 
         img = findViewById(R.id.image_1);
         img.setOnClickListener(new View.OnClickListener() {
@@ -26,5 +42,34 @@ public class buscar_empresa extends AppCompatActivity {
                 startActivity(abrirInfo);
             }
         });
+
+        obtenerEmpresas();
+    }
+
+    private void obtenerEmpresas() {
+        String url = "https://formatoisp-api.herokuapp.com/api/empresa";
+        JsonObjectRequest req = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject res) {
+                try {
+                    JSONArray jsonArr = res.getJSONArray("data");
+                    for (int i = 0; i < jsonArr.length(); i++) {
+                        JSONObject jsonObj = jsonArr.getJSONObject(i);
+                        String empr_nit = jsonObj.getString("empr_nit");
+                        String empr_nombre = jsonObj.getString("empr_nombre");
+                        Toast.makeText(getApplicationContext(),"Nombre", Toast.LENGTH_LONG).show();
+                        Toast.makeText(getApplicationContext(),"Nombre de la empresa: " + empr_nombre, Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        queue.add(req);
     }
 }
