54fc9cf9729db77f39c2a1bc16dc1d7c59fac184
FredyCortes <frcortez@ab-sistemas.com>
2019-09-29 16:43:14 -0500

    Implementando ejemplo de peticion con el metodo POST en el registro empresas

--- app/src/main/java/com/formato/isp/GestionEmpresa/buscar_empresa.java
+++ app/src/main/java/com/formato/isp/GestionEmpresa/buscar_empresa.java
@@ -3,9 +3,7 @@ package com.formato.isp.GestionEmpresa;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -25,15 +23,14 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.PrintWriter;
 import java.util.ArrayList;
 
-public class buscar_empresa extends AppCompatActivity implements  Response.ErrorListener, Response.Listener<JSONObject>{
+public class buscar_empresa extends AppCompatActivity implements Response.ErrorListener, Response.Listener<JSONObject>{
 
     ArrayList<datosEmpresa> dato;
     ImageView img;
-    RequestQueue queue;
-    JsonRequest req;
+    private RequestQueue queue;
+    private JsonRequest req;
     private ListView lvItems;
     private Adaptador adaptador;
 
@@ -78,7 +75,7 @@ public class buscar_empresa extends AppCompatActivity implements  Response.Error
     @Override
     public void onErrorResponse(VolleyError error) {
         if (error instanceof NetworkError) {
-            Toast.makeText(this, "Verifique su conexión a Internet", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Por favor verifica tu conexión a internet", Toast.LENGTH_SHORT).show();
         } else {
             Toast.makeText(this, error.getMessage(), Toast.LENGTH_SHORT).show();
         }
