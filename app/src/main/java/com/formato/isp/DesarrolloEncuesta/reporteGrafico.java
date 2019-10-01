881ad901e5ae9a8d4fa029de4cec69d0705a0947
Huver Acevedo <hacevedo@ab-sistemas.com>
2019-10-01 17:06:57 -0500

    Reporte grafico

--- app/src/main/java/com/formato/isp/DesarrolloEncuesta/reporteGrafico.java
+++ app/src/main/java/com/formato/isp/DesarrolloEncuesta/reporteGrafico.java
@@ -3,107 +3,151 @@ package com.formato.isp.DesarrolloEncuesta;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.graphics.Paint;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-import com.anychart.AnyChart;
-import com.anychart.AnyChartView;
-import com.anychart.chart.common.dataentry.DataEntry;
-import com.anychart.chart.common.dataentry.ValueDataEntry;
-import com.anychart.charts.Pie;
-import com.anychart.charts.Radar;
-import com.anychart.core.radar.series.Line;
-import com.anychart.data.Mapping;
-import com.anychart.data.Set;
-import com.anychart.enums.Align;
-import com.anychart.enums.MarkerType;
-import com.anychart.AnyChart;
-import com.anychart.AnyChartView;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.List;
-import com.formato.isp.R;
 import com.formato.isp.utils.Tools;
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.RadarChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.RadarData;
+import com.github.mikephil.charting.data.RadarDataSet;
+import com.github.mikephil.charting.data.RadarEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IRadarDataSet;
+
+import java.util.ArrayList;
+
+import com.formato.isp.R;
 
 public class reporteGrafico extends AppCompatActivity {
 
+    public static final float MAX = 12, MIN = 1f;
+    public static final int NB_QUALITIES = 5;
+
+    private RadarChart chart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reporte_grafico);
+        initToolbar();
 
-        AnyChartView anyChartView = findViewById(R.id.any_chart_view);
-        anyChartView.setProgressBar(findViewById(R.id.progress_bar));
+        chart = findViewById(R.id.chart1);
+        chart.setBackgroundColor(Color.rgb(60, 65, 82));
+        chart.getDescription().setEnabled(false);
+        chart.setWebLineWidth(1f);
+        chart.setWebColor(Color.WHITE);
+        chart.setWebLineWidth(1f);
+        chart.setWebColor(Color.WHITE);
+        chart.setWebAlpha(100);
+        setTitle("RadarChartActivity");
 
-        Radar radar = AnyChart.radar();
+        setData();
+        chart.animateXY(1400, 1400, Easing.EasingOption.EaseInOutQuad, Easing.EasingOption.EaseInOutQuad);
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setTextSize(9f);
+        xAxis.setYOffset(0);
+        xAxis.setXOffset(0);
+        xAxis.setValueFormatter(new IAxisValueFormatter() {
 
-        radar.title("ESTADISTICA FINAL DE COMPONENTES");
+            private String[] qualities = new String[]{"Comunicacion", "Technical Know", "PRoblem", "Team Player"};
 
-        radar.yScale().minimum(0d);
-        radar.yScale().minimumGap(0d);
-        radar.yScale().ticks().interval(50d);
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                return qualities[(int) value % qualities.length];
+            }
+        });
+        xAxis.setTextColor(Color.WHITE);
+        YAxis yAxis = chart.getYAxis();
+        yAxis.setLabelCount(NB_QUALITIES, false);
+        yAxis.setAxisMinimum(MIN);
+        yAxis.setAxisMaximum(MAX);
+        yAxis.setDrawLabels(false);
 
-        radar.xAxis().labels().padding(5d, 5d, 5d, 5d);
-
-        radar.legend()
-                .align(Align.CENTER)
-                .enabled(true);
-
-        List<DataEntry> data = new ArrayList<>();
-        data.add(new CustomDataEntry("Área1", 136, 199, 43, 54));
-        data.add(new CustomDataEntry("Área2", 79, 125, 56, 65));
-        data.add(new CustomDataEntry("Área3", 149, 173, 101, 122));
-        data.add(new CustomDataEntry("Área4", 135, 33, 202, 222));
-        data.add(new CustomDataEntry("Área5", 158, 64, 196, 212));
-
-        Set set = Set.instantiate();
-        set.data(data);
-        Mapping shamanData = set.mapAs("{ x: 'x', value: 'value' }");
-        Mapping warriorData = set.mapAs("{ x: 'x', value: 'value2' }");
-        Mapping priestData = set.mapAs("{ x: 'x', value: 'value3' }");
-        Mapping gestionData = set.mapAs("{ x: 'x', value: 'value4' }");
-
-        Line shamanLine = radar.line(shamanData);
-        shamanLine.name("Capacitación");
-        shamanLine.markers()
-                .enabled(true)
-                .type(MarkerType.CIRCLE)
-                .size(3d);
-
-        Line warriorLine = radar.line(warriorData);
-        warriorLine.name("Acceso a capital");
-        warriorLine.markers()
-                .enabled(true)
-                .type(MarkerType.CIRCLE)
-                .size(3d);
-
-        Line priestLine = radar.line(priestData);
-        priestLine.name("Construcción de marca");
-        priestLine.markers()
-                .enabled(true)
-                .type(MarkerType.CIRCLE)
-                .size(3d);
-
-        Line gestionLine = radar.line(gestionData);
-        gestionLine.name("Gestión de nuevos mercados");
-        gestionLine.markers()
-                .enabled(true)
-                .type(MarkerType.CIRCLE)
-                .size(3d);
-        radar.tooltip().format("Value: {%Value}");
-
-        anyChartView.setChart(radar);
+        Legend l = chart.getLegend();
+        l.setTextSize(15f);
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        l.setDrawInside(false);
+        l.setXEntrySpace(7f);
+        l.setYEntrySpace(5f);
+        l.setTextColor(Color.WHITE);
     }
 
-    private class CustomDataEntry extends ValueDataEntry {
-        public CustomDataEntry(String x, Number value, Number value2, Number value3, Number value4) {
-            super(x, value);
-            setValue("value2", value2);
-            setValue("value3", value3);
-            setValue("value4", value4);
+    private void initToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbarGrafico);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(null);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Tools.setSystemBarColor(this, R.color.colorPrimary);
+    }
+
+    private void setData() {
+        ArrayList<RadarEntry> employe1 = new ArrayList<>();
+        ArrayList<RadarEntry> employe2 = new ArrayList<>();
+
+        for (int i = 0; i < NB_QUALITIES; i++) {
+            float val1 = (int) (Math.random() * MAX) + MIN;
+            employe1.add(new RadarEntry(val1));
+
+            float val2 = (int) (Math.random() * MAX) + MIN;
+            employe2.add(new RadarEntry(val2));
         }
+        RadarDataSet set1 = new RadarDataSet(employe1, "Employee A");
+        set1.setColor(Color.RED);
+        set1.setFillColor(Color.RED);
+        set1.setDrawFilled(true);
+        set1.setFillAlpha(100);
+        set1.setLineWidth(2f);
+        set1.setDrawHighlightIndicators(false);
+        set1.setDrawHighlightCircleEnabled(true);
+
+        RadarDataSet set2 = new RadarDataSet(employe1, "Employee A");
+        set2.setColor(Color.GREEN);
+        set2.setFillColor(Color.GREEN);
+        set2.setDrawFilled(true);
+        set2.setFillAlpha(100);
+        set2.setLineWidth(2f);
+        set2.setDrawHighlightIndicators(false);
+        set2.setDrawHighlightCircleEnabled(true);
+
+        ArrayList<IRadarDataSet> sets = new ArrayList<>();
+        sets.add(set1);
+        sets.add(set2);
+
+        RadarData data = new RadarData(sets);
+        data.setValueTextSize(8f);
+        data.setDrawValues(false);
+        data.setValueTextColor(Color.WHITE);
+
+        chart.setData(data);
+        chart.invalidate();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.radar, menu);
+        Tools.changeMenuIconColor(menu, getResources().getColor(R.color.colorPrimary));
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        } else {
+            Toast.makeText(getApplicationContext(), item.getTitle(), Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
