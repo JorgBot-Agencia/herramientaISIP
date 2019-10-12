eb596cf414f6e404eff52dbf4d02288323bd2c9c
FredyCortes <frcortez@ab-sistemas.com>
2019-10-12 03:34:53 -0500

    Implementando funcionalidad de generar excel en el reporte gráfico

--- app/src/main/java/com/formato/isp/DesarrolloEncuesta/reporteGrafico.java
+++ app/src/main/java/com/formato/isp/DesarrolloEncuesta/reporteGrafico.java
@@ -8,8 +8,11 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.formato.isp.utils.Tools;
@@ -25,10 +28,21 @@ import com.github.mikephil.charting.data.RadarEntry;
 import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.interfaces.datasets.IRadarDataSet;
 
-import java.lang.reflect.Array;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 
 import com.formato.isp.R;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 
 public class reporteGrafico extends AppCompatActivity {
 
@@ -36,20 +50,24 @@ public class reporteGrafico extends AppCompatActivity {
     public static final int NB_QUALITIES = 4;
 
     private RadarChart chart;
+    private Button generarExcel;
+    private ArrayList<RadarDataSet> areasyCriterios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reporte_grafico);
         initToolbar();
+        areasyCriterios = new ArrayList<>();
+        generarExcel = findViewById(R.id.btnGenerarExcel);
 
         chart = findViewById(R.id.chart1);
-        chart.setBackgroundColor(Color.rgb(60, 65, 82));
+        //chart.setBackgroundColor(Color.rgb(60, 65, 82));
         chart.getDescription().setEnabled(false);
         chart.setWebLineWidth(1f);
-        chart.setWebColor(Color.WHITE);
+        chart.setWebColor(Color.BLACK);
         chart.setWebLineWidth(1f);
-        chart.setWebColor(Color.WHITE);
+        chart.setWebColor(Color.BLACK);
         chart.setWebAlpha(100);
         setTitle("RadarChartActivity");
 
@@ -68,7 +86,7 @@ public class reporteGrafico extends AppCompatActivity {
                 return qualities[(int) value % qualities.length];
             }
         });
-        xAxis.setTextColor(Color.WHITE);
+        xAxis.setTextColor(Color.BLACK);
         YAxis yAxis = chart.getYAxis();
         yAxis.setLabelCount(NB_QUALITIES, false);
         yAxis.setAxisMinimum(MIN);
@@ -83,7 +101,82 @@ public class reporteGrafico extends AppCompatActivity {
         l.setDrawInside(false);
         l.setXEntrySpace(7f);
         l.setYEntrySpace(5f);
-        l.setTextColor(Color.WHITE);
+        l.setTextColor(Color.BLACK);
+
+
+        generarExcel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                guardarExcelGraficoRadar();
+            }
+        });
+    }
+
+    private void guardarExcelGraficoRadar() {
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            //New Workbook
+            Workbook wb = new HSSFWorkbook();
+            CellStyle style = wb.createCellStyle();
+            style.setFillBackgroundColor(IndexedColors.VIOLET.getIndex());
+            style.setFillPattern(CellStyle.SOLID_FOREGROUND);
+            style.setAlignment(CellStyle.ALIGN_CENTER);
+            Cell c = null;
+            //New Sheet
+            Sheet sheet1 = null;
+            sheet1 = wb.createSheet("reporte");
+            // Generate column headings
+            Row row = sheet1.createRow(0);
+            c = row.createCell(0);
+            c.setCellValue("Area");
+            c.setCellStyle(style);
+            c = row.createCell(1);
+            c.setCellValue("25%");
+            c.setCellStyle(style);
+            c = row.createCell(2);
+            c.setCellValue("50%");
+            c.setCellStyle(style);
+            c = row.createCell(3);
+            c.setCellValue("75%");
+            c.setCellStyle(style);
+            c = row.createCell(4);
+            c.setCellValue("100%");
+            c.setCellStyle(style);
+
+            for (int i = 0; i < areasyCriterios.size(); i++) {
+                RadarDataSet rd = areasyCriterios.get(i);
+                row = sheet1.createRow(i+1);
+                c = row.createCell(0);
+                c.setCellValue(rd.getLabel());
+                c = row.createCell(1);
+                c.setCellValue(rd.getEntryForIndex(0).getValue());
+                c = row.createCell(2);
+                c.setCellValue(rd.getEntryForIndex(1).getValue());
+                c = row.createCell(3);
+                c.setCellValue(rd.getEntryForIndex(2).getValue());
+                c = row.createCell(4);
+                c.setCellValue(rd.getEntryForIndex(3).getValue());
+            }
+
+            // Create a path where we will place our List of objects on external storage
+            File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "reporteGrafico" + new Date().toString().replace(" ", "") + ".xls");
+            FileOutputStream os = null;
+
+            try {
+                os = new FileOutputStream(file);
+                wb.write(os);
+                Toast.makeText(this,"Creado en: " + file, Toast.LENGTH_LONG).show();
+            } catch (IOException e) {
+                Toast.makeText(this,"Error al crear " + file, Toast.LENGTH_LONG).show();
+            } catch (Exception e) {
+                Toast.makeText(this,"Falló al guardar " + file, Toast.LENGTH_LONG).show();
+            } finally {
+                try {
+                    if (null != os)
+                        os.close();
+                } catch (Exception ex) {
+                }
+            }
+        }
     }
 
     private ArrayList<RadarEntry> dataValues1(int area){
@@ -134,6 +227,7 @@ public class reporteGrafico extends AppCompatActivity {
     private void setData() {
 
         RadarDataSet set1 = new RadarDataSet(dataValues1(1), "Técnico y productiva");
+        areasyCriterios.add(set1);
         set1.setColor(Color.RED);
         set1.setFillColor(Color.RED);
         set1.setDrawFilled(true);
@@ -143,6 +237,7 @@ public class reporteGrafico extends AppCompatActivity {
         set1.setDrawHighlightCircleEnabled(true);
 
         RadarDataSet set2 = new RadarDataSet(dataValues1(2), "Financiera y administrativa");
+        areasyCriterios.add(set2);
         set2.setColor(Color.GREEN);
         set2.setFillColor(Color.GREEN);
         set2.setDrawFilled(true);
@@ -152,6 +247,7 @@ public class reporteGrafico extends AppCompatActivity {
         set2.setDrawHighlightCircleEnabled(true);
 
         RadarDataSet set3 = new RadarDataSet(dataValues1(3), "Cultura empresarial e innovación");
+        areasyCriterios.add(set3);
         set3.setColor(Color.MAGENTA);
         set3.setFillColor(Color.MAGENTA);
         set3.setDrawFilled(true);
@@ -161,6 +257,7 @@ public class reporteGrafico extends AppCompatActivity {
         set3.setDrawHighlightCircleEnabled(true);
 
         RadarDataSet set4 = new RadarDataSet(dataValues1(4), "Recursos de inversión");
+        areasyCriterios.add(set4);
         set4.setColor(Color.YELLOW);
         set4.setFillColor(Color.YELLOW);
         set4.setDrawFilled(true);
@@ -170,6 +267,7 @@ public class reporteGrafico extends AppCompatActivity {
         set4.setDrawHighlightCircleEnabled(true);
 
         RadarDataSet set5 = new RadarDataSet(dataValues1(5), "Imagen e identidad corporativa");
+        areasyCriterios.add(set5);
         set5.setColor(Color.RED);
         set5.setFillColor(Color.RED);
         set5.setDrawFilled(true);
@@ -179,6 +277,7 @@ public class reporteGrafico extends AppCompatActivity {
         set5.setDrawHighlightCircleEnabled(true);
 
         RadarDataSet set6 = new RadarDataSet(dataValues1(6), "Presentación de producto");
+        areasyCriterios.add(set6);
         set6.setColor(Color.BLACK);
         set6.setFillColor(Color.BLACK);
         set6.setDrawFilled(true);
@@ -188,6 +287,7 @@ public class reporteGrafico extends AppCompatActivity {
         set6.setDrawHighlightCircleEnabled(true);
 
         RadarDataSet set7 = new RadarDataSet(dataValues1(7), "Sellos de calidad");
+        areasyCriterios.add(set7);
         set7.setColor(Color.DKGRAY);
         set7.setFillColor(Color.DKGRAY);
         set7.setDrawFilled(true);
@@ -197,6 +297,7 @@ public class reporteGrafico extends AppCompatActivity {
         set7.setDrawHighlightCircleEnabled(true);
 
         RadarDataSet set8 = new RadarDataSet(dataValues1(8), "Politica de identificación de precios");
+        areasyCriterios.add(set8);
         set8.setColor(Color.GRAY);
         set8.setFillColor(Color.GRAY);
         set8.setDrawFilled(true);
@@ -206,6 +307,7 @@ public class reporteGrafico extends AppCompatActivity {
         set8.setDrawHighlightCircleEnabled(true);
 
         RadarDataSet set9 = new RadarDataSet(dataValues1(9), "Acceso a nuevas tecnologías");
+        areasyCriterios.add(set9);
         set9.setColor(Color.YELLOW);
         set9.setFillColor(Color.YELLOW);
         set9.setDrawFilled(true);
@@ -215,6 +317,7 @@ public class reporteGrafico extends AppCompatActivity {
         set9.setDrawHighlightCircleEnabled(true);
 
         RadarDataSet set10 = new RadarDataSet(dataValues1(10), "Identificación de posibles mercados");
+        areasyCriterios.add(set10);
         set10.setColor(Color.BLUE);
         set10.setFillColor(Color.BLUE);
         set10.setDrawFilled(true);
@@ -224,6 +327,7 @@ public class reporteGrafico extends AppCompatActivity {
         set10.setDrawHighlightCircleEnabled(true);
 
         RadarDataSet set11 = new RadarDataSet(dataValues1(11), "Plan de productividad y empleo");
+        areasyCriterios.add(set11);
         set11.setColor(Color.CYAN);
         set11.setFillColor(Color.CYAN);
         set11.setDrawFilled(true);
@@ -246,7 +350,7 @@ public class reporteGrafico extends AppCompatActivity {
         data.addDataSet(set11);
         data.setValueTextSize(8f);
         data.setDrawValues(false);
-        data.setValueTextColor(Color.WHITE);
+        data.setValueTextColor(Color.BLACK);
 
         chart.setData(data);
         chart.invalidate();
